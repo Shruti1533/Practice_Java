881b757b6391ed92460e18eb719d60c0658e8f30
Shruti1533 <148115188+Shruti1533@users.noreply.github.com>
2024-02-19 20:24:51 +0530

    Time: 8 ms (10.62%) | Memory: 42.9 MB (7.99%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,30 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String m = "";
-        int i = 0;
-
-        if (word1.length() <= word2.length()) {
-            for (i = 0; i < word1.length(); i++) {
-                char b = word1.charAt(i);
-                char c = word2.charAt(i);
-                m = m + b + c;
-            }
-            for (int j = i; j < word2.length(); j++) {
-                char x = word2.charAt(j);
-                m += x;
-            }
-        } else {
-            for (i = 0; i < word2.length(); i++) {
-                char b = word1.charAt(i);
-                char c = word2.charAt(i);
-                m = m + b + c;
-            }
-            for (int j = i; j < word1.length(); j++) {
-                char x = word1.charAt(j);
-                m += x;
-            }
+       int i = 0;
+        String ans = "";
+        while (i < word1.length() && i < word2.length()) {
+            ans += String.valueOf(word1.charAt(i)) + String.valueOf(word2.charAt(i));
+            i++;
         }
 
-        return m;
+        while (i < word1.length()) {
+            ans += String.valueOf(word1.charAt(i));
+            i++;
+        }
+
+        while (i < word2.length()) {
+            ans += String.valueOf(word2.charAt(i));
+            i++;
+        }
+
+        return ans;
     }
 }
